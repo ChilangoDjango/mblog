48fc4c3a2f134252ec0976df8302063e562daa71
Henoc Díaz <henocdz@gmail.com>
2015-06-05 15:06:22 -0500

    Slug uniqueness set to True  - Field setting changed  - Migration created and applied

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Entry(models.Model):
     title = models.CharField(max_length=140)
     content = models.TextField()
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     publish = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
