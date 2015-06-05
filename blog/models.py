ed0f4543157c3541fd58c9da11165577f7b79c32
Henoc Díaz <henocdz@gmail.com>
2015-06-05 14:53:08 -0500

    Entry model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=140)
+    content = models.TextField()
+    slug = models.SlugField()
+    publish = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s - %s".format(self.title, self.modified)
+
