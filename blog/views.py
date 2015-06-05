262e29461e59b8ba0bda9b666ddda95730299ec7
Henoc Díaz <henocdz@gmail.com>
2015-06-05 16:05:51 -0500

    Templates added

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from blog import models as bmodels
 
 class BlogIndex(generic.ListView):
     queryset = bmodels.Entry.objects.filter(publish=True)
-    template_name = 'blog/home.html'
+    template_name = 'blog/index.html'
     paginate_by = 2
