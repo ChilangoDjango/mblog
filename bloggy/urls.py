f8be29ae60c2076f5e6a37d5468620eb88989795
Henoc Díaz <henocdz@gmail.com>
2015-06-05 15:25:40 -0500

    Django Markdown URLs added to default urls.py

--- bloggy/urls.py
+++ bloggy/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'bloggy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^markdown/', include('django_markdown.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
