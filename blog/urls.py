d81f4058e0b7e86664ef085d884d3f581c0ee93f
Henoc Díaz <henocdz@gmail.com>
2015-06-06 11:26:19 -0500

    Articles DetailView   - PostDetail view added   - URL Pattern added   - blog/index.html modified with link to post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from blog import views as bviews
 
 urlpatterns = [
     url(r'^$', bviews.BlogIndex.as_view(), name='index'),
+    url(r'^article/(?P<slug>\w{1,})/$', bviews.PostDetail.as_view(), name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ class BlogIndex(generic.ListView):
     queryset = bmodels.Entry.objects.filter(publish=True)
     template_name = 'blog/index.html'
     paginate_by = 2
+
+
+class PostDetail(generic.DetailView):
+    model = bmodels.Entry
+    template_name = 'blog/post.html'
